eb9110c47d5508c78cd42f08d3af9e3bb5a5c7ef
Darren Cauthon <darren@cauthon.com>
2015-05-15 12:21:57 -0500

    Use the real minus sign.

--- code.rb
+++ code.rb
@@ -1,7 +1,7 @@
 module AddUpTo100
 
   def self.ways
-    ["1 + 2 + 34 – 5 + 67 – 8 + 9"]
+    ["1 + 2 + 34 - 5 + 67 - 8 + 9"]
   end
 
 end
--- code_test.rb
+++ code_test.rb
@@ -7,7 +7,7 @@ describe "find all possible ways to come up with 100 with 1-9, +'s, and -'s" do
   let(:results) { AddUpTo100.ways }
 
   it "should return the first example" do
-    results.include?("1 + 2 + 34 – 5 + 67 – 8 + 9").must_equal true
+    results.include?("1 + 2 + 34 - 5 + 67 - 8 + 9").must_equal true
   end
 
 end
