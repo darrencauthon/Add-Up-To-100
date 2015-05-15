482714cd23c50147fa8fd52cee94c04cddbf6dd0
Darren Cauthon <darren@cauthon.com>
2015-05-15 12:37:55 -0500

    Let's try a different approach.

--- code.rb
+++ code.rb
@@ -1,13 +1,24 @@
 module AddUpTo100
 
   def self.ways
-    (1..9).to_a
-          .map do |n|
-            {
-              num:  n,
-              next: n == 9 ? nil : n + 1,
-            }
-          end
+    next_options_for 1
+    #["1 + 2 + 34 - 5 + 67 - 8 + 9"]
+  end
+
+  def self.next_options_for number
+    next_number = number + 1
+    [
+      "#{number}#{next_number}",
+      "#{number} + #{next_number}",
+      "#{number} - #{next_number}",
+    ].map do |x|
+      {
+        number:      number,
+        next_number: next_number,
+        string:      x,
+        value:       eval(x),
+      }
+    end
   end
 
 end
