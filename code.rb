5132e768b20bfd5fd23bac47ace6c68e2a7708cb
Darren Cauthon <darren@cauthon.com>
2015-05-15 13:17:46 -0500

    Refactor.

--- code.rb
+++ code.rb
@@ -2,30 +2,32 @@ module AddUpTo100
 
   def self.ways
     next_options_for(1)
-      .map { |o| next_options_for o }.flatten
-      .map { |o| next_options_for o }.flatten
-      .map { |o| next_options_for o }.flatten
-      .map { |o| next_options_for o }.flatten
-      .map { |o| next_options_for o }.flatten
-      .map { |o| next_options_for o }.flatten
-      .map { |o| next_options_for o }.flatten
+      .map { |o| next_options_for o[:next_number], o[:statement] }.flatten
+      .map { |o| next_options_for o[:next_number], o[:statement] }.flatten
+      .map { |o| next_options_for o[:next_number], o[:statement] }.flatten
+      .map { |o| next_options_for o[:next_number], o[:statement] }.flatten
+      .map { |o| next_options_for o[:next_number], o[:statement] }.flatten
+      .map { |o| next_options_for o[:next_number], o[:statement] }.flatten
+      .map { |o| next_options_for o[:next_number], o[:statement] }.flatten
       .select { |x| x[:value] == 100 }
       .map { |x| x[:statement] }
   end
 
-  def self.next_options_for number_or_option
-    number_or_option.is_a?(Hash) ? options_for_an_option(number_or_option)
-                                 : options_for_a_number(number_or_option)
-  end
+  #def self.next_options_for number_or_option
+    #number_or_option.is_a?(Hash) ? options_for_an_option(number_or_option)
+                                 #: options_for_a_number(number_or_option)
+  #end
 
-  def self.options_for_a_number number
+  def self.next_options_for number, statement = nil
     next_number = number + 1
+    statement = number unless statement
     [
-      "#{number}#{next_number}",
-      "#{number} + #{next_number}",
-      "#{number} - #{next_number}",
+      "#{statement}#{next_number}",
+      "#{statement} + #{next_number}",
+      "#{statement} - #{next_number}",
     ].map do |x|
       {
+        number:      number,
         next_number: next_number,
         statement:   x,
         value:       eval(x),
@@ -42,6 +44,7 @@ module AddUpTo100
       "#{option[:statement]} - #{next_number}",
     ].map do |x|
       {
+        number:      number,
         next_number: next_number,
         statement:   x,
         value:       eval(x),
