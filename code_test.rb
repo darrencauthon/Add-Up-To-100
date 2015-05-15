02474c77f78f3cf721817c48358bac656d50808c
Darren Cauthon <darren@cauthon.com>
2015-05-15 12:17:24 -0500

    First test.

---
+++ code_test.rb
@@ -0,0 +1,13 @@
+require_relative 'code'
+require 'minitest/autorun'
+require 'minitest/spec'
+
+describe "find all possible ways to come up with 100 with 1-9, +'s, and -'s" do
+
+  let(:results) { AddUpTo100.ways }
+
+  it "should return the first example" do
+    results.include?("1 + 2 + 34 – 5 + 67 – 8 + 9").must_equal true
+  end
+
+end
